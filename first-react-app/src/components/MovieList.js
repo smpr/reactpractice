2f0fdf73ebdb0a007ab0f8e26d3dc598e9a8fbc1
smpr <gcbrown1116@gmail.com>
2017-10-04 15:43:49 -0400

    added inherit values to children

---
+++ first-react-app/src/components/MovieList.js
@@ -0,0 +1,14 @@
+
+  import React, {Component} from 'react';
+  
+    class MovieList extends Component {
+      render() {
+        return (
+            <div>
+              The Movie List
+            </div>
+        );
+      }
+    }
+  
+    export default MovieList;
--- first-react-app/src/components/Review.js
+++ first-react-app/src/components/Review.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
+//import logo from '../logo.svg';
 
 class Review extends Component {
     render() {
       return (
           <div>
             <h1>This is a review page!</h1>
-            <img src={logo} className="App-logo" alt="logo" />
+        
           </div>
       );
     }

4b5df4174be309aab564a88aaa8836c1a60e8bc1
smpr <gcbrown1116@gmail.com>
2017-10-04 15:52:58 -0400

    refactored movies into movieslist

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -5,7 +5,7 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 //this brings in imprted files such as css and js to link it to the code
 import './App.css';
-import MovieList from './components/MovieList';
+import MoviesList from './components/MoviesList';
 import Movie from './components/Movie';
 
 
@@ -15,24 +15,14 @@ class App extends Component {
   // render is ALWAYS used. this says that the following html will be used on screen
   //the <Movie />  brings in the movie.js to show up on the dom where it shows up in the return 
   render() {
+
     
-    const movies = [
-      {title: 'A Movie', rating: 'PG-13', description: 'this is a movie'},
-      {title: 'Another Movie', rating: 'R', description: 'this is another movie'},
-      {title: 'A Third Movie', rating: 'PG', description: 'this is yet another movie'},
-      {title: 'A Fourth Movie', rating: 'PG', description: 'this is yet another movie'},
-      {title: 'A Fifth Movie', rating: 'PG', description: 'this is yet another movie'},
-      {title: 'A Sixth Movie', rating: 'PG', description: 'this is yet another movie'}
-    ];
-    
-          const movieComponents = movies.map((movie) => {
-            return <Movie title={movie.title} rating={movie.rating} description={movie.description}/>;
-          });
+         
     
           return (
             <div>
-              <MovieList />
-              { movieComponents }
+              <MoviesList />
+            
             </div>
           );
     
--- first-react-app/src/components/Review.js
+++ first-react-app/src/components/Review.js
@@ -5,7 +5,7 @@ class Review extends Component {
     render() {
       return (
           <div>
-            <h1>This is a review page!</h1>
+            <h1 class="Movie-test">This is a review page!</h1>
         
           </div>
       );
